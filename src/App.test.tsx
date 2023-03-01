98917a2d6e3fb9c5386e9b65baabe93622c40803
Nigel Faria <nigel.faria@gmail.com>
2023-03-01 15:05:38 +0000

    Activity 3: Mock 500 and 418

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import { wait } from '@testing-library/user-event/dist/utils';
+import { server } from './mocks/server';
+import { rest } from 'msw';
 
 test('check first person is rendered', async () => {
   render(<App />);
@@ -11,6 +13,9 @@ test('check first person is rendered', async () => {
 });
 
 test('check error message on 500', async () => {
+  server.use(rest.get('https://swapi.dev/api/people/', (req, res, ctx) => {
+    return res(ctx.status(500));    
+  }));
   render(<App />);
   await waitFor( ()=> screen.findByText(/Oops.../i));
   const element = screen.getByText(/Oops... something went wrong try again/i);
@@ -18,6 +23,9 @@ test('check error message on 500', async () => {
 });
 
 test('check error message on 418', async () => {
+  server.use(rest.get('https://swapi.dev/api/people/', (req, res, ctx) => {
+    return res(ctx.status(418));    
+  }));
   render(<App />);
   await waitFor( ()=> screen.findByText(/418/i));
   const element = screen.getByText(/418 I'm a tea pot/i);
--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,53 @@ import { SWAPIPerson, Person } from './components/Person';
 
 function App() {
   const [persons, setPersons] = useState<Array<SWAPIPerson>>([]);
+  const [errorCode, setErrorCode] = useState<number>(0);
   useEffect(() => {
     getPersons();
   }, []);
 
   const getPersons = async () => {
-    const apiResponse = await fetch(`https://swapi.dev/api/people/?format=json`);
-    const json = await apiResponse.json() as { results: SWAPIPerson[] };
-    setPersons(json.results);
+    try {
+      const apiResponse = await fetch(`https://swapi.dev/api/people/?format=json`);
+      setErrorCode(apiResponse.status);
+      if (apiResponse.ok){
+        const json = await apiResponse.json() as { results: SWAPIPerson[] };
+        setPersons(json.results);  
+      }
+    }
+    catch (e) {
+      console.error(e);
+      console.log(errorCode);
+    }
+
   };
+
+  if (persons.length > 0) {
+    return (
+      <>
+        <div className="person-container">
+          <Person {...persons[0]} />
+        </div>
+
+      </>
+    );
+  }
+  if (errorCode === 418) {
+    return (
+      <>
+        <div className="person-container">
+        418 I'm a tea pot
+        </div>
+
+      </>
+    );
+  }
 
   return (
     <>
       <div className="person-container">
-        <Person {...persons[0]} />
-      </div>
+        Oops... something went wrong try again
+       </div>
 
     </>
   );
